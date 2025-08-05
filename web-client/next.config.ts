c9e96364b2c96ca9e73d62895d4889b6caa0d3f8
Orjwan Al-Rajaby <orjwan.alrajaby@gmail.com>
2025-08-06 02:29:53 +0300

    configure eslint with prettier to setup formatting scripts

--- web-client/next.config.ts
+++ web-client/next.config.ts
@@ -1,8 +1,12 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [new URL('https://fastly.picsum.photos/id/106/400/400.jpg?hmac=eEZPxCkV47FG3BhZGEEkvju56UXv21b2QNtwjUqcNTc')],
+    remotePatterns: [
+      new URL(
+        'https://fastly.picsum.photos/id/106/400/400.jpg?hmac=eEZPxCkV47FG3BhZGEEkvju56UXv21b2QNtwjUqcNTc'
+      ),
+    ],
   },
 };
 
