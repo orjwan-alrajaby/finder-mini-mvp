ee7b5dce0b968019e6033f33dc33d839aa3c657c
Orjwan Al-Rajaby <orjwan.alrajaby@gmail.com>
2025-08-05 10:16:39 +0300

    build matching responsive Skeletons for the PropertiesList component and the PropertyDetails page

---
+++ web-client/src/app/components/properties/PropertiesList/PropertiesListSkeleton.tsx
@@ -0,0 +1,14 @@
+import PropertyCardSkeleton from "./PropertyCard/PropertyCardSkeleton"
+import { StyledGridList } from "./styled"
+
+export default function PropertiesListSkeleton() {
+  return (
+    <StyledGridList aria-label="Loading properties listing" aria-busy="true">
+      {[...new Array(10).fill((i:number) => i)].map((property) => {
+        return ( 
+          <PropertyCardSkeleton key={property} />
+        )
+      })}
+    </StyledGridList>
+  )
+}
---
+++ web-client/src/app/components/properties/PropertyDetails/Gallery/GallerySkeleton.tsx
@@ -0,0 +1,36 @@
+import styled from 'styled-components';
+
+import {
+  StyledGalleryWrapper as SkeletonGalleryWrapper,
+  StyledMainImageContainer as SkeletonMainImageContainer,
+  StyledThumbnailColumn as SkeletonThumbnailColumn,
+  StyledZoomButton as SkeletonZoomButton,
+  StyledMainFigure,
+  StyledThumbnailItem
+} from "./styled";
+import { getSkeletonStyle } from '@/app/components/layouts/Skeleton/utils';
+
+export const SkeletonMainFigure = styled(StyledMainFigure)<{ $isDark?: boolean }>`
+  background-color: ${({ theme }) => theme.palette.surface};
+  ${({ $isDark }) => getSkeletonStyle($isDark)};
+`;
+
+export const SkeletonThumbnailItem = styled(StyledThumbnailItem)<{ $isDark?: boolean }>`
+  background-color: ${({ theme }) => theme.palette.surface};
+  ${({ $isDark }) => getSkeletonStyle($isDark)};
+`;
+
+export default function SkeletonGallery({ isDark = true }: Readonly<{ isDark?: boolean }>) {
+  return (
+    <SkeletonGalleryWrapper>
+      <SkeletonMainImageContainer>
+        <SkeletonMainFigure $isDark={isDark} />
+        <SkeletonZoomButton aria-hidden />
+      </SkeletonMainImageContainer>
+      <SkeletonThumbnailColumn>
+        <SkeletonThumbnailItem $isDark={isDark} />
+        <SkeletonThumbnailItem $isDark={isDark} />
+      </SkeletonThumbnailColumn>
+    </SkeletonGalleryWrapper>
+  );
+}
